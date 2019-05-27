f369e69c6e08b568622ff2ed1cf972951e5ed813
Tim McMaster <tim@mcmaster.id.au>
2019-05-27 15:45:23 +1000

    first version of element

--- demo/index.js
+++ demo/index.js
@@ -11,11 +11,22 @@ render(html`
       background-color: #ededed;
     }
     div { 
+        display: inline-block;
         width: 100px;
         height: 50px;
+        background: lightgray;
+        padding:10px;
+        margin-bottom: 5px;
     }
   </style>
   
-  <tm-hand-drawn-outline><div>:-)</div></tm-hand-drawn-outline>
+  <tm-hand-drawn-outline>
+    <div>:-)</div>
+    <div>:-)</div>
+    <div>:-)</div>
+    <div>:-)</div>
+    <div>:-)</div>
+    <div>:-)</div>
+  </tm-hand-drawn-outline>
   
 `, document.querySelector('body'));
