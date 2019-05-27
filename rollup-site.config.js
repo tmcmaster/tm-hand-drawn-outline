441db5b7d728844c4e84ebeb550cd8aa2636a921
Tim McMaster <tim@mcmaster.id.au>
2019-05-27 14:19:14 +1000

    clearing the release branch

--- rollup-site.config.js
+++
@@ -1,9 +0,0 @@
-import createDefaultConfig from '@open-wc/building-rollup/modern-config';
-
-// noinspection JSUnusedGlobalSymbols
-export default createDefaultConfig(
-    {
-        input: 'demo/index.js',
-        outputDir: 'dist/site'
-    }
-);
